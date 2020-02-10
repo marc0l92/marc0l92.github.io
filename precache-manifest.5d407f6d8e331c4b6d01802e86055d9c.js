922db9cbb01f07983933baa87ca6a003eaad124a
Marco Lucarella <lucarellamarco@yahoo.it>
2020-02-10 01:55:59 +0100

    First version

---
+++ precache-manifest.5d407f6d8e331c4b6d01802e86055d9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2bc269e0e351f3cba7b121d580de9ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71fc2a0cb2818a208570",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "79472664bc383d69adde",
+    "url": "/static/js/2.5802e494.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5802e494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71fc2a0cb2818a208570",
+    "url": "/static/js/main.f89998db.chunk.js"
+  },
+  {
+    "revision": "610c6b96a3347ab8b806",
+    "url": "/static/js/runtime-main.81bff0bb.js"
+  }
+]);
